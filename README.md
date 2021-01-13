54c722bb7d77a1c0fafe0645ef4a2eeb348b0c7d
samhyun <rnflal31@gmail.com>
2021-01-13 12:04:00 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# querydsl-study
+# querydsl-study
---
+++ querydsl-study-java/src/main/java/com/example/querydsl/study/QuerydslStudyJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.querydsl.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuerydslStudyJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuerydslStudyJavaApplication.class, args);
+    }
+
+}
